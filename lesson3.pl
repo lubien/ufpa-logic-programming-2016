e22ff02eb896078af06b4711ca0b502bdd4e1bf8
lubien <lubien1996@gmail.com>
2016-11-09 16:21:12 -0300

    SAVEPOINT

---
+++ lesson3.pl
@@ -0,0 +1,32 @@
+% Calculate list entries
+comprAcc(L, C) :- compr3(L, 0, C).
+
+compr3([X | Xs], Acc, C) :- compr3(Xs, Acc + 1, C).
+compr3([], Acc, Result) :- Result = Acc.
+
+compr(L, C) :- comprOther(L, 0, C).
+
+comprOther([], Acc, Result) :- Result is Acc.
+comprOther([X | Xs], Acc, Result) :- comprOther(Xs, Acc + 1, Result).
+
+% Sum list of values
+sum([], 0).
+sum([X | Xs], X + S) :- sum(Xs, S).
+
+sum1([], 0).
+sum1([X | Xs], S) :- sum1(Xs, S1), S is S1 + X.
+
+% Find in list
+member(X, [X | _]).
+member(X, [_ | Xs]) :- member(X, Xs).
+
+member1(X, L) :- append(_, [X | _], L).
+
+% Append
+append1([], Bs, Bs).
+append1([A | As], Bs, [A | AsBs]) :- append(As, Bs, AsBs).
+
+% Prefix, Suffix and Sublist
+prefix(P, L) :- append(P, _, L).
+suffix(S, L) :- append(_, S, L).
+sublist(S, l) :- prefix(P, L), suffix(S, P).
